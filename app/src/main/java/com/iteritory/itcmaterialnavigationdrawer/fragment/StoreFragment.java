b8f810f005388937f232f3a441c4b4b1dfa7da31
Sadruddin Mohammed <suavesalman@gmail.com>
2017-10-27 22:00:26 +0530

    initial commit

---
+++ app/src/main/java/com/iteritory/itcmaterialnavigationdrawer/fragment/StoreFragment.java
@@ -0,0 +1,36 @@
+package com.iteritory.itcmaterialnavigationdrawer.fragment;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.iteritory.itcmaterialnavigationdrawer.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StoreFragment extends Fragment {
+
+
+    public StoreFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_store, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Store");
+    }
+}
